02f64fdd04bf55d36793cba43ce67557f19d38c1
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-10-14 23:23:24 +0300

    feat: add app

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>app</div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/app';
+
 ReactDOM.render(
-  <div>Hello!!!</div>,
+  <App />,
   document.getElementById('root')
 );
