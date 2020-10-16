5db510456ec4b92809d63c5961926b1c50e59cd9
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-10-16 19:10:44 +0300

    feat: add styles to components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {  Switch, Route, Link } from 'react-router-dom';
 
+import ShopHeader from '../shop-header';
 import HomePage from '../pages/home-page';
 import CartPage from '../pages/cart-page';
 
@@ -8,17 +9,18 @@ import './app.css';
 
 const App = () => {
   return (
-    <div>
-      <ul>
-        <li><Link to='/'>home</Link></li>
-        <li><Link to='/cart'>cart</Link></li>
+    <main role='main' className='container'>
+      <ShopHeader numItems={5} total={210} />
+      <ul className='row'>
+        <li className='btn btn-secondary'><Link to='/'>home</Link></li>
+        <li className='btn btn-secondary'><Link to='/cart'>cart</Link></li>
       </ul>
       <Switch>
         <Route path='/' exact component={HomePage} />
         <Route path='/cart' component={CartPage} />
         <Route render={() => <div>404 Not Found</div>} />
       </Switch>
-    </div>
+    </main>
   );
 }
 
--- src/components/pages/home-page/home-page.js
+++ src/components/pages/home-page/home-page.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import ShoppingCartTable from '../../shopping-cart-table/shopping-cart-table';
 import BookList from '../../book-list';
 
 import './home-page.css';
 
 const HomePage = () => {
   return (
-    <BookList />
+    <div>
+      <BookList />
+      <ShoppingCartTable />
+    </div>
   );
 }
 
